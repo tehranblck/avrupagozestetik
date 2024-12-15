914d4df89f9496b92383ae089cfc0cb65b0fe15f
Tehran Bayramov <nicosspyros@gmail.com>
2024-12-15 04:21:13 +0400

    sfgfsd

--- app/[locale]/home/layout.tsx
+++ app/[locale]/home/layout.tsx
@@ -1,7 +1,5 @@
 import Header from '@/components/Header/Header';
 import React, { ReactNode } from 'react';
-import { fetchCategories } from '@/components/fetchCategories';
-import { getLocale } from 'next-intl/server';
 import HeaderBottom from '@/components/Header/HeaderBottom';
 
 interface LayoutProps {
@@ -9,12 +7,11 @@ interface LayoutProps {
 }
 
 const Layout: React.FC<LayoutProps> = async ({ children }) => {
-    const locale = await getLocale();
-    const categories = await fetchCategories(locale);
+
 
     return (
         <div >
-            <div style={{ borderBottomLeftRadius: '20px', borderBottomRightRadius: '20px' }} className=' w-full fixed top-0 right-0 !pb-3'>
+            <div style={{ borderBottomLeftRadius: '20px', borderBottomRightRadius: '20px' }} className=' w-full fixed top-0 right-0 '>
                 <Header />
                 <HeaderBottom />
             </div>
--- components/Header/HeaderBottom.tsx
+++ components/Header/HeaderBottom.tsx
@@ -1,7 +1,6 @@
 'use client';
 import React from 'react';
 import { Link } from '@/i18n/routing';
-import Image from 'next/image';
 import { MdCategory } from "react-icons/md";
 
 const HeaderBottom = () => {

8a5b0ff7d563796a90bce3195c55c216c2b91091
Tehran Bayramov <nicosspyros@gmail.com>
2024-12-31 21:45:07 +0400

    x aded

--- components/SixPhoto/PopupPhoto.tsx
+++ components/SixPhoto/PopupPhoto.tsx
@@ -71,14 +71,14 @@ const PopupPhoto: React.FC<PopupProps> = ({
         >
             <div
                 ref={contentRef}
-                className="relative bg-white mt-12 p-1 sm:p-6 md:p-8 lg:p-10 xl:p-12 rounded-lg shadow-lg w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] max-w-7xl"
+                className="relative bg-red-800 mt-12 p-1 pt-10 sm:p-6 md:p-8 lg:p-10 xl:p-12 rounded-lg shadow-lg w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] max-w-7xl"
                 onClick={(e) => e.stopPropagation()} // Pop-up içindeki tıklamayı engeller
             >
                 {/* Close Button */}
                 <button
                     onClick={onClose}
-                    style={{ color: 'black' }}
-                    className="absolute top-0 right-0 text-black rounded-full hover:text-black text-4xl md:text-3xl focus:outline-none"
+                    style={{ color: 'white' }}
+                    className="absolute top-0 right-0 text-white   rounded-full  text-3xl md:text-3xl focus:outline-none"
                     aria-label="Close Popup"
                 >
                     ✖

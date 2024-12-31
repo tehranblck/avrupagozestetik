f9b8e50250ece902a87cf7d9248f5afca4c45eae
Tehran Bayramov <nicosspyros@gmail.com>
2024-12-31 21:33:12 +0400

    m

--- components/AskQuestionButton/AskQuestionButton.tsx
+++ components/AskQuestionButton/AskQuestionButton.tsx
@@ -1,9 +1,10 @@
 'use client';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaWhatsapp } from 'react-icons/fa';
 
 const AskQuestionButton: React.FC = () => {
     const phoneNumber = '+905327044102'; // WhatsApp numarası
+    const [isButtonVisible, setIsButtonVisible] = useState(true); // Buton görünürlüğü durumu
 
     // WhatsApp'a yönlendirme işlemi
     const handleWhatsAppRedirect = () => {
@@ -12,17 +13,38 @@ const AskQuestionButton: React.FC = () => {
         window.open(whatsappUrl, '_blank'); // Yeni sekmede WhatsApp'a yönlendirme
     };
 
+    // Scroll durumunu kontrol etme
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY >= 1200) {
+                setIsButtonVisible(false); // 1200px'den fazla scroll yapıldığında buton gizlenir
+            } else {
+                setIsButtonVisible(true); // 1200px'den az scroll yapıldığında buton görünür
+            }
+        };
+
+        // Scroll event listener'ı ekliyoruz
+        window.addEventListener('scroll', handleScroll);
+
+        // Cleanup işlemi
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <div>
             {/* WhatsApp Butonu */}
-            <div className="fixed bottom-4 right-4 z-50">
-                <button
-                    onClick={handleWhatsAppRedirect} // Butona tıkladığında WhatsApp'a yönlendirme yapılır
-                    className="bg-green-500 text-white px-5 py-3 flex items-center gap-3 font-medium rounded-lg shadow-lg border border-green-600 hover:bg-green-600 transition-all duration-300"
-                >
-                    WhatsApp <FaWhatsapp className="text-2xl" />
-                </button>
-            </div>
+            {isButtonVisible && (
+                <div className="fixed bottom-4 right-4 z-50">
+                    <button
+                        onClick={handleWhatsAppRedirect} // Butona tıkladığında WhatsApp'a yönlendirme yapılır
+                        className="bg-green-500 text-white px-5 py-3 flex items-center gap-3 font-medium rounded-lg shadow-lg border border-green-600 hover:bg-green-600 transition-all duration-300"
+                    >
+                        WhatsApp <FaWhatsapp className="text-2xl" />
+                    </button>
+                </div>
+            )}
         </div>
     );
 };
